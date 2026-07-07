0eded5aca4737a0fad33142018b90dceced27762
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-07 23:33:23 +0500

    feat: add DoctorResponse schema for returning doctor data to client

--- app/schemas/doctor.py
+++ app/schemas/doctor.py
@@ -7,3 +7,18 @@ class DoctorCreate(BaseModel):
     email: EmailStr
     phone: Optional[str] = None
     salary: Optional[float] = 0.0
+
+class DoctorUpdate(BaseModel):
+    name: Optional[str] = None
+    specialization: Optional[str] = None
+    email: Optional[EmailStr] = None
+    phone: Optional[str] = None
+    salary: Optional[float] = None
+
+class DoctorResponse(BaseModel):
+    id: int
+    name: str
+    specialization: str
+    email: str
+    phone: Optional[str] = None
+    salary: float
