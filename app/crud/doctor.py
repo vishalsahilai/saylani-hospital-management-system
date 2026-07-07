e2a3d092e981565d9ccf4a334db41de2c6ca89cd
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-08 00:10:09 +0500

    feat: add create_doctor function to save new doctor in database

--- app/crud/doctor.py
+++ app/crud/doctor.py
@@ -1,3 +1,10 @@
 from sqlalchemy.orm import Session
 from app.models.doctor import Doctor
 from app.schemas.doctor import DoctorCreate, DoctorUpdate
+
+def create_doctor(db: Session, data: DoctorCreate) -> Doctor:
+    doctor = Doctor(**data.model_dump())
+    db.add(doctor)
+    db.commit()
+    db.refresh(doctor)
+    return doctor
