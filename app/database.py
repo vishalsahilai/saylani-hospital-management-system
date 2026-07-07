29d5beaac878e94a5ae84dfbc6905be0b773fa87
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-07 22:50:37 +0500

    connection between Python and your MySQL database

--- app/database.py
+++ app/database.py
@@ -7,3 +7,7 @@ DATABASE_URL = (
     f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}"
     f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 )
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
