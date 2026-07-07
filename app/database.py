6ab421eb4ee2cb7c084e731b12198af23b853fb4
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-07 22:46:52 +0500

    DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -2,3 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
+
+DATABASE_URL = (
+    f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
