dd594177cf153e2689005908ed4ac769d671a848
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-08 00:24:57 +0500

    feat: add create_patient function to save new patient in database

--- app/crud/patient.py
+++ app/crud/patient.py
@@ -1,3 +1,10 @@
 from sqlalchemy.orm import Session
 from app.models.patient import Patient
 from app.schemas.patient import PatientCreate, PatientUpdate
+
+def create_patient(db: Session, data: PatientCreate) -> Patient:
+    patient = Patient(**data.model_dump())
+    db.add(patient)
+    db.commit()
+    db.refresh(patient)
+    return patient
