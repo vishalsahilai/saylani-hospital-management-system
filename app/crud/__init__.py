955c193976f0b98da8678d5ff3ad05f5989cd181
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-08 01:14:52 +0500

    feat: register all crud functions in package init

---
+++ app/crud/__init__.py
@@ -0,0 +1,3 @@
+from .doctor import create_doctor, get_doctor, get_all_doctors, update_doctor, delete_doctor
+from .patient import create_patient, get_patient, get_all_patients, update_patient, delete_patient
+from .staff import create_staff, get_staff, get_all_staff, update_staff, delete_staff
