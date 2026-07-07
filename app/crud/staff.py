82eb2c1c11704dddccdb76ced0999d1b3e5c1cf8
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-08 00:46:00 +0500

    feat: add create_staff function to save new staff in database

--- app/crud/staff.py
+++ app/crud/staff.py
@@ -2,3 +2,9 @@ from sqlalchemy.orm import Session
 from app.models.staff import Staff
 from app.schemas.staff import StaffCreate, StaffUpdate
 
+def create_staff(db: Session, data: StaffCreate) -> Staff:
+    staff = Staff(**data.model_dump())
+    db.add(staff)
+    db.commit()
+    db.refresh(staff)
+    return staff
