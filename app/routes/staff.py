6a888776fc95b1f0cb44640835ff706bb40d4ad8
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-08 15:43:54 +0500

    feat: add staff router and post endpoint to create new staff

--- app/routes/staff.py
+++ app/routes/staff.py
@@ -3,3 +3,10 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas.staff import StaffCreate, StaffUpdate, StaffResponse
 from app.crud.staff import create_staff, get_staff, get_all_staff, update_staff, delete_staff
+
+router = APIRouter(prefix="/staff", tags=["Staff"])
+
+
+@router.post("/", response_model=StaffResponse)
+def add_staff(data: StaffCreate, db: Session = Depends(get_db)):
+    return create_staff(db, data)
