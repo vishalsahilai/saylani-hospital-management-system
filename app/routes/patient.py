0b7faf7899d0cfe699a76d0a16068e5194f4f011
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-08 15:25:35 +0500

    feat: add patient router and post endpoint to create new patient

--- app/routes/patient.py
+++ app/routes/patient.py
@@ -3,3 +3,10 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas.patient import PatientCreate, PatientUpdate, PatientResponse
 from app.crud.patient import create_patient, get_patient, get_all_patients, update_patient, delete_patient
+
+router = APIRouter(prefix="/patient", tags=["Patient"])
+
+
+@router.post("/", response_model=PatientResponse)
+def add_patient(data: PatientCreate, db: Session = Depends(get_db)):
+    return create_patient(db, data)
