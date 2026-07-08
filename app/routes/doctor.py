e47749698fde0b404d2e201724b38200e731ca78
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-08 15:14:28 +0500

    feat: add doctor router and post endpoint to create new doctor

--- app/routes/doctor.py
+++ app/routes/doctor.py
@@ -3,3 +3,10 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas.doctor import DoctorCreate, DoctorUpdate, DoctorResponse
 from app.crud.doctor import create_doctor, get_doctor, get_all_doctors, update_doctor, delete_doctor
+
+router = APIRouter(prefix="/doctor", tags=["Doctor"])
+
+
+@router.post("/", response_model=DoctorResponse)
+def add_doctor(data: DoctorCreate, db: Session = Depends(get_db)):
+    return create_doctor(db, data)
